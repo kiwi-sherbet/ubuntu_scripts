7ba8671094b7aaf19e3df3a2c2be30310beee82f
Mingyo Seo <mingyo@utexas.edu>
2024-03-06 21:26:56 -0600

    ubuntu 22.04

--- basic.sh
+++ basic.sh
@@ -7,7 +7,7 @@ sudo apt install -y vim
 sudo apt install -y wget git curl llvm wget
 sudo apt install -y make build-essential 
 sudo apt install -y libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev 
-sudo apt install -y python-openssl
+sudo apt install -y python3-openssl
 
 
 PYENV_ROOT="$HOME/.pyenv"
@@ -21,11 +21,11 @@ git clone https://github.com/pyenv/pyenv-virtualenv.git $PYENV_VENV
 sudo sh ./third_party/linux-brprinter-installer-2.2.3-1
 
 mkdir ./tmp
-wget https://developer.download.nvidia.com/compute/cuda/11.1.0/local_installers/cuda_11.1.0_455.23.05_linux.run -P ./tmp
-sudo sh ./tmp/cuda_11.1.0_455.23.05_linux.run
+# wget https://developer.download.nvidia.com/compute/cuda/11.1.0/local_installers/cuda_11.1.0_455.23.05_linux.run -P ./tmp
+# sudo sh ./tmp/cuda_11.1.0_455.23.05_linux.run
 
-# wget https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.2.1.32/11.3_06072021/cudnn-11.3-linux-x64-v8.2.1.32.tgz -P ./tmp
-# tar -xzvf ./tpm/cudnn-11.3-linux-x64-v8.2.1.32.tgz -C ./tmp
+#wget https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.2.1.32/11.3_06072021/cudnn-11.3-linux-x64-v8.2.1.32.tgz -P ./tmp
+# tar -xzvf ./tmp/cudnn-11.3-linux-x64-v8.2.1.32.tgz -C ./tmp
 # sudo cp ./tmp/cuda/include/cudnn*.h /usr/local/cuda/include
 # sudo cp -P ./tmp/cuda/lib64/libcudnn* /usr/local/cuda/lib64 
 # sudo chmod a+r /usr/local/cuda/include/cudnn*.h /usr/local/cuda/lib64/libcudnn*
